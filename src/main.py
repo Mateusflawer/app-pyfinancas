33529bf2ee4fa0274cbf18b3c90492434090faed
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2024-09-12 21:57:56 -0300

    atualização

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import locale
 locale.setlocale(locale.LC_ALL, "")
 
 def main():
-    st.switch_page("pages\\2_dashboard.py")    
+    st.switch_page("pages\\0_login.py")    
 
 if __name__ == "__main__":
     main()
