cc0ba3b91adaffb477e34392d6ce3d8de6cf7042
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2024-09-17 20:59:20 -0300

    at

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import streamlit as st
+from utils import instalador
 import locale
 
 locale.setlocale(locale.LC_ALL, "")
 
+# Chamar a função para instalar os pacotes
 def main():
+    instalador.install_requirements()
     st.switch_page("pages/0_login.py")    
     
 if __name__ == "__main__":
