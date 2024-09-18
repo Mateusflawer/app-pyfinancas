f58e746bcfb0076f714d81066358b9748b2e72cd
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2024-09-17 21:05:00 -0300

    instalador

--- utils/instalador.py
+++ utils/instalador.py
@@ -8,4 +8,5 @@ def install_requirements():
         subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
         print("Todos os pacotes instalados com sucesso!")
     except subprocess.CalledProcessError as e:
-        print(f"Erro ao instalar os pacotes: {e}")
+        print(f"Erro ao instalar os pacotes: {e}")  
+    
